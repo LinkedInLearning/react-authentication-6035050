933734a88236c4f202bd5026e14a0de54ba1e938
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-17 20:30:13 +0000

    The rest of the setup code

--- back-end/db.js
+++ back-end/db.js
@@ -1,13 +1,19 @@
-export let database = {};
+const fs = require('fs');
+const path = require('path');
 
-const DB_FILE = 'database.json';
+const DB_FILE = path.join(__dirname, 'db.json');
 
 if (fs.existsSync(DB_FILE)) {
-  database = JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
+  db = JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
 } else {
-  database = { users: [] };
+  db = { users: [] };
 }
 
-export const saveDatabase = () => {
-  fs.writeFileSync(DB_FILE, JSON.stringify(database, null, 2), 'utf8');
+const saveDb = () => {
+  fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2), 'utf8');
 };
+
+module.exports = {
+  db,
+  saveDb,
+}
--- back-end/server.js
+++ back-end/server.js
@@ -1,21 +1,8 @@
 const express = require('express');
-const fs = require('fs');
 
 const app = express();
 app.use(express.json());
 
-const DB_FILE = 'database.json';
-
-let database = {};
-
-if (fs.existsSync(DB_FILE)) {
-  database = JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
-} else {
-  database = { users: [] };
-}
-
-export const saveDatabase = () => {
-  fs.writeFileSync(DB_FILE, JSON.stringify(database, null, 2), 'utf8');
-};
+// Endpoints go here
 
 app.listen(3000, () => console.log('Server running on port 3000'));

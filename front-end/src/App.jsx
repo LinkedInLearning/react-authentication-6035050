7a90fa98b3fce75737bbb9313782e6d543a59a48
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-30 18:55:48 +0000

    Added route to app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,6 +6,7 @@ import { UserInfoPage } from './UserInfoPage';
 import { PrivateRoute } from './PrivateRoute';
 import { useUser } from './useUser';
 import { PleaseVerifyEmailPage } from './PleaseVerifyEmailPage';
+import { EmailVerificationLandingPage } from './EmailVerificationLandingPage';
 
 function App() {
   const user = useUser();
@@ -17,6 +18,7 @@ function App() {
           <Route path="/log-in" element={<LogInPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/please-verify" element={<PleaseVerifyEmailPage />} />
+          <Route path="/verify-email" element={<EmailVerificationLandingPage />} />
           <Route element={<PrivateRoute redirectPath="log-in" isAllowed={!!user} />}>
             <Route path="/" element={<UserInfoPage />} />
           </Route>

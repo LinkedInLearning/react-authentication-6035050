49e94bf3430f5450d2d95fae66e939ef608c0499
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-30 19:25:31 +0000

    Added page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,6 +7,7 @@ import { PrivateRoute } from './PrivateRoute';
 import { useUser } from './useUser';
 import { PleaseVerifyEmailPage } from './PleaseVerifyEmailPage';
 import { EmailVerificationLandingPage } from './EmailVerificationLandingPage';
+import { ForgotPasswordPage } from './ForgotPasswordPage';
 
 function App() {
   const user = useUser();
@@ -19,6 +20,7 @@ function App() {
           <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/please-verify" element={<PleaseVerifyEmailPage />} />
           <Route path="/verify-email/:verificationString" element={<EmailVerificationLandingPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
           <Route element={<PrivateRoute redirectPath="log-in" isAllowed={!!user} />}>
             <Route path="/" element={<UserInfoPage />} />
           </Route>

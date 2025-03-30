66d6f1a65ac70a30d0796f56e7dab19ee17f92f0
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-30 18:40:39 +0000

    Added route from signup page

--- front-end/src/SignUpPage.jsx
+++ front-end/src/SignUpPage.jsx
@@ -21,7 +21,7 @@ export const SignUpPage = () => {
     });
     const { token } = response.data;
     setToken(token);
-    navigate('/', { replace: true });
+    navigate('/please-verify', { replace: true });
   }
 
   return (
